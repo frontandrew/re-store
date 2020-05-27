c145a9d00845fa80228afe1d30cf8a40727f2fa7
Andrew Bubnov <jackblack.nt@ya.ru>
2020-05-27 19:39:33 +1100

    UI components has been added.

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+
+import BookList from '../book-list';
 
 const HomePage = () => {
+  const books = [
+    {
+      id: 1,
+      title: 'Production-Ready Microservices',
+      author: 'Susan J. Fowler',
+    },
+    {
+      id: 2,
+      title: 'Release It!',
+      author: 'Michael T. Nygard',
+    },
+  ];
+
   return (
-    <h2>Home</h2>
+    <Fragment>
+      <h2>Home</h2>
+      <BookList books={books} />
+    </Fragment>
   );
 }
 
