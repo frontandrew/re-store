529f349785533533361cf51b9750347bf4c251c4
Andrew Bubnov <jackblack.nt@ya.ru>
2020-05-28 05:11:42 +1100

    Updates

---
+++ precache-manifest.fd50032c803e511ac34e135212cc9771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c9fc6606f1aee5b25f42cbf17ea3c8",
+    "url": "/re-store/index.html"
+  },
+  {
+    "revision": "8b66c2135c48637d1b93",
+    "url": "/re-store/static/css/main.832a614d.chunk.css"
+  },
+  {
+    "revision": "cb3afb41569b69c3c193",
+    "url": "/re-store/static/js/2.b390a1e9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/re-store/static/js/2.b390a1e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b66c2135c48637d1b93",
+    "url": "/re-store/static/js/main.1345abd2.chunk.js"
+  },
+  {
+    "revision": "cb5a188f76acfcd690f8",
+    "url": "/re-store/static/js/runtime-main.d2fb03b7.js"
+  }
+]);
