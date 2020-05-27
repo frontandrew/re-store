e51936c4ed5fb05690ef77537145c21c8ccf646a
Andrew Bubnov <jackblack.nt@ya.ru>
2020-05-28 05:54:59 +1100

    Updates

--- precache-manifest.91b3deb64d0d05cf53efeb3908f77475.js
+++ precache-manifest.91b3deb64d0d05cf53efeb3908f77475.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fa2ed3e4c518f634cce56cbef95994a4",
+    "revision": "acfc47cf2d0b6774d98fa5281e9301f2",
     "url": "/re-store/index.html"
   },
   {
-    "revision": "0d4a398f66ffa7f7c0c9",
+    "revision": "8ee76718cfeb1150edbf",
     "url": "/re-store/static/css/main.832a614d.chunk.css"
   },
   {
-    "revision": "cb3afb41569b69c3c193",
-    "url": "/re-store/static/js/2.b390a1e9.chunk.js"
+    "revision": "cd1aaf273f7b70249f0d",
+    "url": "/re-store/static/js/2.4ba66894.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/re-store/static/js/2.b390a1e9.chunk.js.LICENSE.txt"
+    "url": "/re-store/static/js/2.4ba66894.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0d4a398f66ffa7f7c0c9",
-    "url": "/re-store/static/js/main.c9e66c9c.chunk.js"
+    "revision": "8ee76718cfeb1150edbf",
+    "url": "/re-store/static/js/main.df1175e0.chunk.js"
   },
   {
     "revision": "cb5a188f76acfcd690f8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/re-store/precache-manifest.5742727e399ed08ab8a926083147e405.js"
+  "/re-store/precache-manifest.91b3deb64d0d05cf53efeb3908f77475.js"
 );
 
 self.addEventListener('message', (event) => {
